d9c435a6cdcc5fade5b7c050bf6512e575aee83b
AnimeshGupta1 <animeshicpc24@gmail.com>
2026-03-02 14:51:44 +0530

    Vertical Slice Structure Begins

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id {get; set;}
+    public String Name {get; set;} = default!;
+    public List<String> Category {get; set;} = new ();
+    public String Description {get; set;} = default!;
+    public String ImageFile {get;set;} = default!;
+    public decimal Price {get; set;}
+}
