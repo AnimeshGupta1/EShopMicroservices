e3573c4f6c0e6979ff559581d5858905727605cb
AnimeshGupta1 <animeshicpc24@gmail.com>
2026-03-03 18:22:59 +0530

    Added Mapping Library

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,8 +1,20 @@
 using System;
+using Carter;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductEndpoint
-{
+// captures all the information to create a product
+// we need to map the request to CommandQuery using Mapping library (Mapster)
+public record CreateProductRequest(string Name, List<String>Category, string Description, string ImageFile, decimal Price);
 
+// returns the final result
+public record CreateProductResponse(Guid id);
+
+// we need to send values from request to trigger the handler
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
 }
